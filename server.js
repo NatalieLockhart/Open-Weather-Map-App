66132a7f92295cd398bd27dd2511f3144761afae
NL <natalie.lockhart5@gmail.com>
2019-01-19 10:08:25 -0700

    Adding OpenWeather API call
    Adding a simple axios API call to server.js

--- server.js
+++ server.js
@@ -10,3 +10,16 @@ app.get('/weather', function(req, res) {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+var apiKey = '7bf5d1bb8a756b8177afbb2ee2e3be3e';
+var zipCode = '80223';
+
+//todo: switch to xml
+axios.get(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=${apiKey}`)
+  .then(response => {
+	console.log(response.data.list[0].main.temp);
+  })
+  .catch(error => {
+    console.log(error);
+  });
